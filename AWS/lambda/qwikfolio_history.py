103e89f625e5888b5b63ce889f3fe0b9a87d561e
DavidNettleship <34097148+DavidNettleship@users.noreply.github.com>
2021-02-23 18:49:07 +0000

    qwikfolio_history lambda - continued

--- AWS/lambda/qwikfolio_history.py
+++ AWS/lambda/qwikfolio_history.py
@@ -15,6 +15,7 @@ def lambda_handler(event, context):
     data = []
     currency = 'GBP'
     
+    #Fetch current prices
     for item in assets['Items']:
     
         #coingecko api
@@ -24,6 +25,8 @@ def lambda_handler(event, context):
             req = pd.DataFrame.from_dict(req.json()) 
             rq=req.copy()
             rq['ticker']=ticker
+            rq['type']=item['class']
+            rq['quant']=item['quantity']
             data.append(rq.to_json(orient='columns'))
 
         #yfinance api
@@ -31,12 +34,56 @@ def lambda_handler(event, context):
             ticker = item['ticker']
             close = yf.download(item['api_name'], period="1d")["Close"]
             cl=close.copy()
-            cl['ticker']=ticker
-            cl['price']=cl[0]
+            cl['ticker'] = ticker
+            cl['type'] = item['class']
+            cl['quant']=item['quantity']
+            
+            #UK stocks
+            if item['api_name'][-2:] == '.L':
+                cl['price']=round(cl[0]/100,2)
+
+            #US stocks
+            else:
+                cl['price']=round(cl[0]/1.41,2) #TODO: Make exchange rate dynamic
+
             data.append(cl.to_json(orient='columns'))
 
+    #Current values
     for dat in data:
-        print(dat)
+        asset = dat.split(",")
+
+        #Get stock price
+        if asset[2] == "\"type\":\"stock\"":
+
+            raw_ticker = asset[1].split(":")
+            ticker = raw_ticker[1].strip('\"').rstrip('\"')
+
+            raw_quant = asset[3].split(":")
+            quant = raw_quant[1]
+
+            raw_price = asset[4].split(":")
+            price = raw_price[1].rstrip('}')
+
+            value = round(float(price)*float(quant),2)
+            
+            print(ticker)
+            print(value)
+        
+        elif asset[2] == "\"type\":{\"gbp\":\"crypto\"}":
+
+            raw_ticker = asset[1].split(":")
+            ticker = raw_ticker[2].strip('\"').rstrip('\"}')
+
+            raw_quant = asset[3].split(":")
+            quant = raw_quant[2].rstrip('}')
+
+            raw_price = asset[0].split(":")
+            price = raw_price[2].rstrip('}')
+
+            value = round(float(price)*float(quant),2)
+
+            print(ticker)
+            print(value)
 
 #for testing
 lambda_handler(1,2)
